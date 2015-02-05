97cec7a1ebae33dbb17ed1dfcc65811ade780690
Jeremy Nelson <jermnelson.gmail.com>
2015-02-05 16:46:26 -0700

    added version file and now supports running as falcon REST api

--- app.py
+++ app.py
@@ -1,5 +1,35 @@
+__author__ = "Jeremy Nelson"
+__license__ = "GPLv3"
+
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+with open(os.path.join(BASE_DIR, "VERSION")) as version:
+    __version__ = version.read().strip()
+
+import importlib
 import asyncio
+import falcon
 import sys
 from asyncio import subprocess
+semantic_server = importlib.import_module("semantic-server.app", None)
 
+class StartUp(object):
 
+    def on_get():
+        pass
+
+    def on_post():
+        pass
+
+    def on_delete():
+        pass
+
+# Add BIBFRAME specific REST API
+##semantic_server.api.add_route("/Work")
+##semantic_server.api.add_route("/Annotation")
+##semantic_server.api.add_route("/Authority")
+##semantic_server.api.add_route("/Instance")
+
+if __name__ == "__main__":
+    semantic_server.main()

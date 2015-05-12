0b79ff3260709271d358eb4533f86836e205ed14
Jeremy Nelson <jermnelson.gmail.com>
2015-05-12 17:25:56 -0600

    Continued working on Setup for project

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 __author__ = "Jeremy Nelson"
 
 from setuptools import setup
+from setuptools.command.install import install
 import os
+import re
+import sys
+import subprocess
+
+
 
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 BASE_DIR = os.path.abspath(os.path.dirname(CURRENT_DIR))
@@ -12,13 +18,40 @@ if os.path.exists(os.path.join(BASE_DIR, "VERSION")):
 with open(version_filepath) as version:
     __version__ = version.read().strip()
 
+
+
+class BIBFRAMEDatastoreInstall(install):
+    JAVA_CHECK = re.compile(r"java version \"(.+)\"")
+
+    def __check_java__(self):
+        """Method checks for a Java 7+ JVM to run other dependencies"""
+        raw_output = subprocess.getoutput("java -version") 
+        result = BIBFRAMEDatastoreInstall.JAVA_CHECK.search(raw_output)
+        if result is not None:
+            version = result.groups()[0].split(".")
+            if int(version[1]) >= 7:
+                return True
+        return False
+
+    def __install_dependency__(self, **kwargs):
+        pass
+
+    def run(self):
+        install.run(self)
+        if not self.__check_java__():
+            raise ValueError("Missing Java or Java version < 7")
+        
+        
+
 setup(
     name='bibframe_datastore',
     author=__author__,
     author_email='jermnelson@gmail.com',
+    cmdclass={'install': BIBFRAMEDatastoreInstall},
     description='BIBFRAME Datastore Installation',
     url='https://github.com/jermnelson/bibframe-datastore',
     install_requires=[
+        'elasticsearch',
         'falcon',
         'flask', 
         'rdflib',
@@ -32,8 +65,7 @@ setup(
               'semantic web'],
     py_modules=['app'],
     version=__version__,
-    packages=['.',
-              'semantic_server',
+    packages=['semantic_server',
               'semantic_server.analytics',
               'semantic_server.analytics.resources',
               'semantic_server.analytics.utilities',
